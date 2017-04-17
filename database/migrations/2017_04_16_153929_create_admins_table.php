f30941312229b06be30c81ce0f74a3597817ec99
dashajin <dashajin@126.com>
2017-04-17 13:22:09 +0800

    修改admins数据迁移文件

--- database/migrations/2017_04_16_153929_create_admins_table.php
+++ database/migrations/2017_04_16_153929_create_admins_table.php
@@ -15,6 +15,10 @@ class CreateAdminsTable extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
