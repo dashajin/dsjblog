f3d77d85d8de8248bb3197adde1c1b0e5c325b6a
dashajin <dashajin@126.com>
2017-04-19 00:32:50 +0800

    添加passwords里的admins

--- config/auth.php
+++ config/auth.php
@@ -107,6 +107,11 @@ return [
             'table' => 'password_resets',
             'expire' => 60,
         ],
+        'admins' => [
+            'provider' => 'admins',
+            'table' => 'password_resets',
+            'expire' => 60,
+        ],
     ],
 
 ];
